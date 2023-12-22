1413f48f373410fffbbdcec87b2c6c5554ec05da
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-22 22:52:58 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,6 @@ import { AuthProvider } from './context/AuthContext.tsx';
 import axios from 'axios';
 import {Toaster} from 'react-hot-toast';
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
-axios.defaults.withCredentials = true;
-const  theme = createTheme({
-  typography: {
-    fontFamily : "Roboto Slab,serif",
-    allVariants: {color: "white"}, 
-  }
-})  
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <Toaster position = "top-right"/>
-    <App />
-    </ThemeProvider>
-    </BrowserRouter></AuthProvider>
-  </React.StrictMode>,
-)
-
 
 axios.defaults.baseURL = "https://mern-gpt.azurewebsites.net/api/v1";
 axios.defaults.withCredentials = true;
