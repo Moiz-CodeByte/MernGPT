5d692a7a8a33fb4bb1a9f261d07e1bfbea536658
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-22 22:43:09 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
+import App from '../app.tsx'
 import './index.css';
 import {BrowserRouter} from "react-router-dom"
 import { createTheme, ThemeProvider } from '@mui/material'
-import { AuthProvider } from './src/context/AuthContext.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 import axios from 'axios';
 import {Toaster} from 'react-hot-toast';
+
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.withCredentials = true;
+const  theme = createTheme({
+  typography: {
+    fontFamily : "Roboto Slab,serif",
+    allVariants: {color: "white"}, 
+  }
+})  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Toaster position = "top-right"/>
+    <App />
+    </ThemeProvider>
+    </BrowserRouter></AuthProvider>
+  </React.StrictMode>,
+)
+
 
 axios.defaults.baseURL = "https://mern-gpt.azurewebsites.net/api/v1";
 axios.defaults.withCredentials = true;
