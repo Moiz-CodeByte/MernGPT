dfb514a0bffa0899ce5076300447069c32ac5197
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-28 00:31:36 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+
+//   plugins: [react()],
+// })
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Update with your actual app name or leave it as '/' if your app is at the root.
   plugins: [react()],
   server: {
-    port: process.env.PORT, // Change this to the desired port number
+    proxy: {
+      "/api" : "https://mern-gpt.azurewebsites.net"
+    }
   }
-})
+  // server: {
+  //   port: process.env.PORT, // Specify the port for the development server (optional, defaults to 3000)
+  //   open: true, // Open the default browser when the development server starts (optional, defaults to false)
+   
+  //   }
+  });
