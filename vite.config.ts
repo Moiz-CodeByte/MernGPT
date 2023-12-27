73b7e76670e3d989874693e0714a8c73b9d428ef
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-28 01:28:23 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,13 @@ export default defineConfig({
     proxy: {
       "/api" : "https://mern-gpt.azurewebsites.net"
     }
-  }
+    
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'main.tsx'), // Adjust the path accordingly
+      },
   // server: {
   //   port: process.env.PORT, // Specify the port for the development server (optional, defaults to 3000)
   //   open: true, // Open the default browser when the development server starts (optional, defaults to false)
