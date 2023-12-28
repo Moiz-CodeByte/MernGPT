767f3888180b3c8fb4d6a842cca54512ca55cbb9
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-28 17:20:47 +0500

    Update ChatItem.tsx

--- src/compenents/chat/ChatItem.tsx
+++ src/compenents/chat/ChatItem.tsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Typography } from '@mui/material';
 import { useAuth } from '../../context/AuthContext'
-import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { coldarkDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function extractCodeFromString(message: string) {
