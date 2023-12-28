8552f8ea5f98970a1145c4eadd98f8f340faef3c
Abdul Moiz <111345413+Moiz-CodeByte@users.noreply.github.com>
2023-12-28 17:38:40 +0500

    Update logo.tsx

--- src/compenents/shared/logo.tsx
+++ src/compenents/shared/logo.tsx
@@ -1,5 +1,5 @@
-import {Link} from "react-router-dom";
-import { Typography } from "@mui/material";
+import {Link} from 'react-router-dom';
+import { Typography } from '@mui/material';
 const Logo = () => {
     return <div style={{
         display: "flex" , 
